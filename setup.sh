538e7155bda209cc9e94960e7b667b778469a680
root <root@localhost.localdomain>
2019-03-11 05:00:34 -0400

    installation script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+
+# Check if user is root
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+echo '[+] Running: apt-get update -y'
+apt-get update -y
+
+echo '[+] Downloading dirsearch repository in /opt folder'
+cd /opt
+git clone https://github.com/maurosoria/dirsearch.git
+
+echo '[+] Downloading & Installing penetest-machine repository in /opt folder'
+cd /opt
+git clone https://github.com/DanMcInerney/pentest-machine.git
+cd pentest-machine
+./setup.sh
+
+echo '[+] Downloading & Installing net-creds repository in /opt folder'
+cd /opt
+git clone https://github.com/DanMcInerney/net-creds.git
+cd net-creds
+pip install -r requirements.txt
+
+echo '[+] Congratulations, All tools installed successfully!'
+echo '[+] Done. When you are done running this tool, cd into pentest-machine and deactivate the virtualenv by running the command: deactivate'
